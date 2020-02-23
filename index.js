165baa217636f528744db8ab6e55a0034143e7e4
Aholap <patrik.ahola@hotmail.com>
2020-02-23 17:54:05 +0200

    procfile

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ require('dotenv').config()
 
 
 
-server.listen(process.env.PORT, () => {
-    console.log(`Server running on port` + process.env.PORT)
+server.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on port ` + process.env.PORT)
   })
