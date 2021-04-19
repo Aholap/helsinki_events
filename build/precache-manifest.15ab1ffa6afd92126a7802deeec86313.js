6de37f70e8ada387710dc7c2c45c221342e2f880
Aholap <patrik.ahola@hotmail.com>
2021-04-19 13:55:25 +0300

    frontend search filter

---
+++ build/precache-manifest.15ab1ffa6afd92126a7802deeec86313.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3995dd6e1a579ee22cc3ec1c6fddd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac4ea4454cdb504d93a2",
+    "url": "/static/css/main.4f38fb2a.chunk.css"
+  },
+  {
+    "revision": "3b5c062c1039cf0c7551",
+    "url": "/static/js/2.2c288312.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2c288312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac4ea4454cdb504d93a2",
+    "url": "/static/js/main.a9821f5c.chunk.js"
+  },
+  {
+    "revision": "33340f59695f4a50c356",
+    "url": "/static/js/runtime-main.50f64c01.js"
+  },
+  {
+    "revision": "45d0b04fd8c76f816399b948dc554432",
+    "url": "/static/media/helsinki_pic.45d0b04f.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7fa1fe3ad4835d803bb73bfa32ec9868.js"
+  "/precache-manifest.15ab1ffa6afd92126a7802deeec86313.js"
 );
 
 self.addEventListener('message', (event) => {
